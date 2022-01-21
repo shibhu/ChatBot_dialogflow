f605d154e2fce22698497445bc47ac51151d318e
Kshitij Chawla <34532111+shibhu@users.noreply.github.com>
2022-01-21 21:13:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ def webhook():
 
 # processing the request from dialogflow
 def processRequest(req):
+    log = logger.Log()
+
+    sessionID=req.get('responseId')
+
 
     result = req.get("queryResult")
     user_says=result.get("queryText")
@@ -41,12 +45,27 @@ def processRequest(req):
     cust_email=parameters.get("Peron_email")
     cust_date= parameters.get("Person_date")
     intent = result.get("intent").get('displayName')
-    
-    fulfillmentText="We have send the details to the team. Please wait you will recieve the confirmation on mail"
-    log.write_log(sessionID, "Bot Says: "+fulfillmentText)
-    return {
-        "fulfillmentText": fulfillmentText
-    }
+    if (intent=='Booking'):
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+  
+        # start LS for security
+        s.starttls()
+        # Authentication
+        s.login("***@gmail.com", "*****")
+        # message to be sent
+        message = "Your Booking for " + cust_name +" has been confirmed for "+cust_date
+        # sending the mail
+        s.sendmail("kchawla.cse15@gmail.com", cust_email, message)
+        # terminating the session
+        s.quit() 
+        fulfillmentText="We have send the details to the team. Please wait you will recieve the confirmation on mail"
+        log.write_log(sessionID, "Bot Says: "+fulfillmentText)
+        return {
+            "fulfillmentText": fulfillmentText
+        }
+    else:
+        log.write_log(sessionID, "Bot Says: " + result.fulfillmentText)
+
 
 if __name__ == '__main__':
     app.run()
